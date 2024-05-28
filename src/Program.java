52fdb1bcf90a8c0e6cf1d3b885bcd9d80df92bf2
pompos02 <yiannis.caravellas@gmail.com>
2024-05-28 12:10:58 +0300

    Programme renamed to Program

--- src/Program.java
+++ src/Program.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 import static OutputQueries.Run2DQueries.rStarTreeMaker;
 
-public class Programme {
+public class Program {
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
